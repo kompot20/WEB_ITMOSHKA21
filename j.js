96b3a27fe740d26d8decd38169260f7522215169
kompot20 <90027963+kompot20@users.noreply.github.com>
2021-12-29 16:29:51 +0300

    Update j.js

--- j.js
+++ j.js
@@ -1,9 +1,6 @@
 (function pageSpeed() {
-    const startTime = new Date();
-
     window.onload = function() {
-        const endTime = new Date();
-        const latency = (endTime - startTime) / 1000;
+        const latency = performance.now() / 1000;
         const latencyBlock = document.querySelector('.latency');
 
         latencyBlock.innerHTML = 'Load time is ' + latency + ' sec';
